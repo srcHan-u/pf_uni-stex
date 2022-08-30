5182eac662a4daa182c0537dc907f0b3e5053278
Han <boikoale97@gmail.com>
2022-08-30 23:00:11 +0300

    #1 created first-screen

--- src/js/app.js
+++ src/js/app.js
@@ -147,7 +147,7 @@ import * as flsForms from "./files/forms/forms.js";
 // Документация по работе в шаблоне: https://template.fls.guru/template-docs/modul-lenivaya-podgruzka-lazy-loading.html
 // Документация плагина: https://github.com/verlok/vanilla-lazyload
 // Сниппет(HTML):
-// import './files/scroll/lazyload.js';
+import './files/scroll/lazyload.js';
 
 // Наблюдатель за объектами c атрибутом data-watch
 // Документация: https://template.fls.guru/template-docs/modul-nabljudatel-za-poyavleniem-elementa-pri-skrolle.html

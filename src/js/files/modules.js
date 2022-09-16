8aa5816305c941ce3c96da307d546db3fb43c917
Han <boikoale97@gmail.com>
2022-09-16 15:54:32 +0300

    feat: added animations for all blocks
    added animations using libraries which i added

--- src/js/files/modules.js
+++ src/js/files/modules.js
@@ -1,1 +1,2 @@
-export const flsModules = {}
+export const flsModules = {
+}
--- src/js/files/script.js
+++ src/js/files/script.js
@@ -2,3 +2,5 @@
 import { isMobile } from "./functions.js";
 // Подключение списка активных модулей
 import { flsModules } from "./modules.js";
+
+import './anim.js'
